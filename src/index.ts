89d87fe5f868b76646173d160806542ed13030e4
Robert Pandele <r.pandele38@gmail.com>
2023-01-03 14:23:45 +0200

    feat: finished the api

--- src/index.ts
+++ src/index.ts
@@ -10,32 +10,102 @@ app.use(express.urlencoded({ extended: true }));
 
 // get all posts
 app.get("/posts", async (req, res, next) => {
+    try {
+        const posts = await prisma.post.findMany({
+            where: {
+                published: true,
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
 
+        res.json({ posts });
+    } catch (error: any) {
+        next(error.message);
+    }
 });
 
 // create a post
 app.post("/posts", async (req, res, next) => {
+    try {
+        const post = await prisma.post.create({
+            data: {
+                authorId: 1, ...req.body
+            }
+        });
 
+        res.json({ post });
+    } catch (error: any) {
+        next(error.message);
+    }
 });
 
 // get post by id
 app.get("/posts/:id", async (req, res, next) => {
-
+    try {
+        const post = await prisma.post.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+        
+        res.json({ post });
+    } catch (error: any) {
+        next(error.message);
+    }
 });
 
 // update a post
 app.patch("/posts/:id", async (req, res, next) => {
+    try {
+        const post = await prisma.post.update({
+            where: {
+                id: parseInt(req.params.id),
+            },
+            data: req.body,
+        })
 
+        res.json({ post });
+    } catch (error: any) {
+        next(error.message);
+    }
 });
 
 // delete a post
 app.delete("/posts/:id", async (req, res, next) => {
-
+    try {
+        await prisma.post.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+    } catch (error: any) {
+        next(error.message);
+    }
 });
 
 // get a user's posts
 app.get("/users/:id/posts", async (req, res, next) => {
+    try {
+        const userWithPosts = await prisma.user.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            include: {
+                posts: {
+                    where: {
+                        published: true,
+                    },
+                },
+            }
+        });
 
+        const posts = userWithPosts?.posts;
+        res.json({ posts });
+    } catch (error: any) {
+        next(error.message);
+    }
 });
 
 app.listen(3000, () => {
